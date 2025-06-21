5eff7ace1d8ceb9e03c3a231e778da7cc94ebcf9
Lokesh Esehat <lokesh.dalvi@esehat.org>
2025-06-21 15:02:42 +0530

    On CLick

--- static/src/components/main_dashboard.js
+++ static/src/components/main_dashboard.js
@@ -99,6 +99,37 @@ export class MainDashboard extends Component {
           ],
         },
       };
+
+      this.state.bar_chart = {
+        type: "bar",
+        title: "State-wise Record Count",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "No. of Records",
+              data: counts,
+              fill: false,
+              tension: 0.3,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 205, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+              ],
+              borderColor: [
+                "rgb(255, 99, 132)",
+                "rgb(255, 159, 64)",
+                "rgb(255, 205, 86)",
+                "rgb(75, 192, 192)",
+                "rgb(54, 162, 235)",
+              ],
+              borderWidth: 2,
+            },
+          ],
+        },
+      };
     } catch (error) {
       console.error("Error loading dashboard data:", error);
       this.state.dashboardData = null;
